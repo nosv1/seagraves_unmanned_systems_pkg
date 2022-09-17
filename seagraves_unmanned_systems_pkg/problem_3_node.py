16016b707adb164675a008a1842f07fded9e0cef
Chris Seagraves <christopher.seagraves@gmail.com>
2022-09-16 22:48:28 -0500

    we move, we turn, we move, we stop...

--- seagraves_unmanned_systems_pkg/problem_3_node.py
+++ seagraves_unmanned_systems_pkg/problem_3_node.py
@@ -1,28 +1,49 @@
-import rclpy
-from rclpy.node import Node
+
+import time
 
 from geometry_msgs.msg import Twist
+import rclpy
+from rclpy.node import Node
 
 class Problem3Node(Node):
     def __init__(self):
         super().__init__('publisher_node')
         self.publisher_ = self.create_publisher(Twist, "/cmd_vel", 10)
 
-    def publish_speed(self, speed):
+    def publish_speed(self, speed: float):
         msg = Twist()
         msg.linear.x = speed
         self.publisher_.publish(msg)
         self.get_logger().info(f"Publishing speed: {speed}")
 
+    def publish_rotation(self, rotation: float):
+        msg = Twist()
+        msg.angular.z = rotation
+        self.publisher_.publish(msg)
+        self.get_logger().info(f"Publishing rotation: {rotation}")
+        
+    def move(self, speed: float, duration: float):
+        now: float = time.perf_counter()
+        while time.perf_counter() < now + duration:
+            self.publish_speed(speed)
+            rclpy.spin_once(self, timeout_sec=0.1)
+
+    def turn(self, rotation: float, duration: float):
+        now: float = time.perf_counter()
+        while time.perf_counter() < now + duration:
+            self.publish_rotation(rotation)
+            rclpy.spin_once(self, timeout_sec=0.1)
+
 def main():
     rclpy.init()
 
     publisher_node = Problem3Node()
-
-    speed = 1.5
-    while rclpy.ok():
-        publisher_node.publish_speed(speed)
-        rclpy.spin_once(publisher_node)
+    
+    publisher_node.move(1.5, 5.0)
+    publisher_node.turn(-.15, 2)
+    publisher_node.move(1.5, 5.0)
+    
+    publisher_node.publish_speed(0.0)
 
     publisher_node.destroy_node()
     rclpy.shutdown()
