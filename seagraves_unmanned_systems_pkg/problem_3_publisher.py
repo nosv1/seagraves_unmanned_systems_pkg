28a1aeb4579e3d7c58f5033c6754bc095a3bbd19
Chris Seagraves <christopher.seagraves@gmail.com>
2022-09-18 14:29:17 -0500

    no more stopping to turn

--- seagraves_unmanned_systems_pkg/problem_3_publisher.py
+++ seagraves_unmanned_systems_pkg/problem_3_publisher.py
@@ -1,4 +1,3 @@
-
 import time
 
 from geometry_msgs.msg import Twist
@@ -9,48 +8,44 @@ class CmdVelPublisher(Node):
     def __init__(self):
         super().__init__('publisher_node')
         self.publisher_ = self.create_publisher(Twist, "/cmd_vel", 10)
+        self.twist = Twist()
 
     def publish_speed(self, speed: float):
-        msg = Twist()
-        msg.linear.x = speed
-        self.publisher_.publish(msg)
+        self.twist.linear.x = speed
+        self.publisher_.publish(self.twist)
         self.get_logger().info(f"Publishing speed: {speed}")
 
     def publish_rotation(self, rotation: float):
-        msg = Twist()
-        msg.angular.z = rotation
-        self.publisher_.publish(msg)
+        self.twist.angular.z = rotation
+        self.publisher_.publish(self.twist)
         self.get_logger().info(f"Publishing rotation: {rotation}")
         
-    def move(self, speed: float, duration: float):
-        now: float = time.perf_counter()
-        self.publish_speed(speed)
-        rclpy.spin_once(self, timeout_sec=0.1)
-        while time.perf_counter() < now + duration:
-            continue
-        # self.publish_speed(0.0)
-        # rclpy.spin_once(self, timeout_sec=0.1)
+    def move(self, speed: float, duration: float, stop: bool = False):
+        now = time.perf_counter()
+        while time.perf_counter() - now < duration:
+            self.publish_speed(speed)
+            rclpy.spin_once(self, timeout_sec=0.1)
+        if stop:
+            self.publish_speed(0.0)
+            rclpy.spin_once(self, timeout_sec=0.1)
 
-    def turn(self, rotation: float, duration: float):
-        now: float = time.perf_counter()
-        self.publish_rotation(rotation)
-        rclpy.spin_once(self, timeout_sec=0.1)
-        while time.perf_counter() < now + duration:
-            continue
-        self.publish_rotation(0.0)
-        rclpy.spin_once(self, timeout_sec=0.1)
+    def turn(self, rotation: float, duration: float, stop: bool = False):
+        now = time.perf_counter()
+        while time.perf_counter() - now < duration:
+            self.publish_rotation(rotation)
+            rclpy.spin_once(self, timeout_sec=0.1)
+        if stop:
+            self.publish_rotation(0.0)
+            rclpy.spin_once(self, timeout_sec=0.1)
 
 def main():
     rclpy.init()
 
     publisher_node = CmdVelPublisher()
     
-    while rclpy.ok():
-        publisher_node.move(1.5, 5.0)
-        publisher_node.turn(-.15, 2)
-        publisher_node.move(1.5, 5.0)
-        publisher_node.publish_speed(0.0)
-        break
+    publisher_node.move(1.5, 5.0)
+    publisher_node.turn(-.15, 2, stop=True)
+    publisher_node.move(1.5, 5.0, stop=True)
 
     publisher_node.destroy_node()
     rclpy.shutdown()
