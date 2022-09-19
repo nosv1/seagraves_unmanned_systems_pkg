5648385721388566877bafa6fc70da4396c696d5
Chris Seagraves <christopher.sesagraves@gmail.com>
2022-09-18 19:27:42 -0500

    wip trying to use sim time

--- seagraves_unmanned_systems_pkg/problem_3_publisher.py
+++ seagraves_unmanned_systems_pkg/problem_3_publisher.py
@@ -1,3 +1,5 @@
+import time
+
 from geometry_msgs.msg import Twist
 import rclpy
 from rclpy.node import Node
@@ -11,30 +13,46 @@ class CmdVelPublisher(Node):
     def publish_speed(self, speed: float):
         self.twist.linear.x = speed
         self.publisher_.publish(self.twist)
-        self.get_logger().info(f"Publishing speed: {speed}")
 
     def publish_rotation(self, rotation: float):
         self.twist.angular.z = rotation
         self.publisher_.publish(self.twist)
-        self.get_logger().info(f"Publishing rotation: {rotation}")
         
     def move(self, speed: float, duration: float, stop: bool = False):
+        self.get_logger().info(f"Moving @ {speed}m/s for {duration}s...")
+        
         duration_ns: float = duration * 1e9
-        now_ns: float = self.get_clock().now().nanoseconds
-        while self.get_clock().now().nanoseconds - now_ns < duration_ns:
+        sim_now_ns: int = self.get_clock().now().nanoseconds
+        pc_now_ns: float = time.time_ns()
+
+        while self.get_clock().now().nanoseconds - sim_now_ns < duration_ns:
             self.publish_speed(speed)
             rclpy.spin_once(self, timeout_sec=0.1)
+            
+        self.get_logger().info(f"Sim Ellapsed: {self.get_clock().now().nanoseconds - sim_now_ns}")
+        self.get_logger().info(f"PC Ellapsed: {time.time_ns() - pc_now_ns}")
+
         if stop:
+            self.get_logger().info("Moving stopped...")
             self.publish_speed(0.0)
             rclpy.spin_once(self, timeout_sec=0.1)
 
     def turn(self, rotation: float, duration: float, stop: bool = False):
+        self.get_logger().info(f"Turning @ {rotation}rad/s for {duration}s...")
+        
         duration_ns: float = duration * 1e9
-        now_ns: float = self.get_clock().now().nanoseconds
-        while self.get_clock().now().nanoseconds - now_ns < duration_ns:
+        sim_now_ns: int = self.get_clock().now().nanoseconds
+        pc_now_ns: float = time.time_ns()
+
+        while self.get_clock().now().nanoseconds - sim_now_ns < duration_ns:
             self.publish_rotation(rotation)
             rclpy.spin_once(self, timeout_sec=0.1)
+            
+        self.get_logger().info(f"Sim Ellapsed: {self.get_clock().now().nanoseconds - sim_now_ns}")
+        self.get_logger().info(f"PC Ellapsed: {time.time_ns() - pc_now_ns}")
+            
         if stop:
+            self.get_logger().info("Turning stopped...")
             self.publish_rotation(0.0)
             rclpy.spin_once(self, timeout_sec=0.1)
 
